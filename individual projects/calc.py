969e366998b935229d7b0c55f7946d5752188177
Deanrobert-dell <dean.peterson@ucas-edu.net>
2026-01-09 10:37:44 -0700

    gdgfdfvzvzx

--- individual projects/calc.py
+++ individual projects/calc.py
@@ -1,21 +1,31 @@
 import math
 import turtle
-def save():
+"""def save():
 
 def interest():
 
-def budget():
+def budget():"""
 
 def sale():
+    price = int(input("what is the original price of the item: "))
+    sale = int(input("what percentage is the discount: "))
+    discount_amount = (sale/100) * price
+    final_price = price - discount_amount
+    fixed = round(final_price, 2)
+    print(fixed)
 
 def tip():
-    bill = input("what is the price of item your buying: ")
-    tip = input("what percentage are you tipping: ")
+    bill = int(input("what is the price of item your buying: "))
+    tip = int(input("what percentage are you tipping: "))
     tipp = 1+(tip/100)
     final = bill * tipp
-tip()
+    fixed = round(final, 2)
+    print(fixed)
+
 
 print("this is a financial calculator")
 choice = input("do you want to use: 1. Savings Time Calculator, 2. Compound Interest Calculator, 3. Budget Allocator, 4. Sale Price Calculator, 5. Tip Calculator. enter corresponding calculator: ")
-if choice == 1:
+if choice == "5":
     tip()
+elif choice == "4":
+    sale()
---
+++ individual projects/tempCodeRunnerFile.py
@@ -0,0 +1,5 @@
+def save():
+
+def interest():
+
+def budget():
