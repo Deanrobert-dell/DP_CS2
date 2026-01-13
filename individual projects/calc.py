f2878a16fe92d74fbacb71d9d29500db5298dc1d
Deanrobert-dell <dean.peterson@ucas-edu.net>
2026-01-12 23:03:23 -0700

    Refactor calc.py to include savings calculator
    Removed unused functions and added savings calculator.

--- individual projects/calc.py
+++ individual projects/calc.py
@@ -1,10 +1,5 @@
 import math
-import turtle
-"""def save():
 
-def interest():
-
-def budget():"""
 
 def sale():
     price = int(input("what is the original price of the item: "))
@@ -50,6 +45,18 @@ def compound():
     fixed = round(final, 2)
     print("your amount after", time, "units of time is:", fixed)
     
+def savings():
+    amount = int(input("how much are you saving for: "))
+    new = input(" 1 for weely 2 for monthly: ")
+    mon = input('how much are you putting in each time: ')
+    if new == 1:
+        final1 = mon * 4
+        equate = amount / mon
+        print("it will take", equate, "months to save enough")
+    elif new == 2:
+        equate2 = amount / mon
+        print ("it will take", equate2, "months to save enough")
+        
 
 print("this is a financial calculator")
 choice = input("do you want to use: 1. Savings Time Calculator, 2. Compound Interest Calculator, 3. Budget Allocator, 4. Sale Price Calculator, 5. Tip Calculator. enter corresponding calculator: ")
@@ -61,3 +68,5 @@ elif choice == "3":
     budget()
 elif choice == "2":
     compound()
+elif choice == "1":
+    savings()
