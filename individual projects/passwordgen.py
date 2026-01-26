27cd9b925cb480876e2f45b00163ae8ed12215c5
Deanrobert-dell <dean.peterson@ucas-edu.net>
2026-01-26 10:51:47 -0700

    ertjhy,kfdgdmbnxhggdh

---
+++ individual projects/passwordgen.py
@@ -0,0 +1,7 @@
+#DP 2nd password generator
+
+def main(): 
+    #intoduce to genorator
+    print("Welcome to a password generotor")
+    choice = input("Do you want to gen password(1), exit(2), ")
+    
