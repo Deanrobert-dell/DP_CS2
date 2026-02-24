53c44e0b390a625cf186395b1f4c86834030f44a
Deanrobert-dell <dean.peterson@ucas-edu.net>
2026-02-24 21:41:24 +0000

    superduper2123123

--- wordcount/time.py
+++ wordcount/time.py
@@ -1,3 +1,11 @@
+from datetime import datetime
+
+# Get the current date and time
+current_datetime = datetime.now()
+
+# Print the result
+print("Current date and time:", current_datetime)
+
 
 
 def record_time(doc_id, duration):
