af4029188bbda5d1604cfea4a44c4d36c54972a2
Deanrobert-dell <dean.peterson@ucas-edu.net>
2026-02-25 10:07:45 -0700

    gdfbtfdxgnbfdrgb

--- wordcount/README.md
+++
@@ -1,1 +0,0 @@
-#did things that were done
--- wordcount/view.py
+++
@@ -1,6 +0,0 @@
-#display
-def display_document(content):
-    #simply displays in a clean way
-    print("\nDocument content:")
-    print(content)
-    print()
