aa8732251d1725dba2c8bc7f2dcd780cf66de16f
Deanrobert-dell <dean.peterson@ucas-edu.net>
2026-02-25 10:04:35 -0700

    agdsrfdagdsbdas

--- wordcount/append.py
+++ wordcount/append.py
@@ -1,7 +1,1 @@
-
-
-def add_to_document(doc_id, content):
-    """Append content to a document and return True on success."""
-    # implement append logic here
-    return True
-
+#merged with file file
--- wordcount/view.py
+++ wordcount/view.py
@@ -1,11 +1,6 @@
-
-
-# Prints documents in good way
-def print_docs(docs):
-    for d in docs:
-        print(
-            f'Title: "{d["title"]}"  ' #here and below have space to seperate ethe feild better, prints it by colums
-            f'Genres: {(d["genres"]).title()}  '
-            f'Director: {d["director"].title()}  '
-            f'Length: {d["length"]} min'
-        )
+#display
+def display_document(content):
+    #simply displays in a clean way
+    print("\nDocument content:")
+    print(content)
+    print()
