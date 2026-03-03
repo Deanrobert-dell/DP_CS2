e04bf2fa722fa8d79f5ee0e452293839a238addf
Deanrobert-dell <dean.peterson@ucas-edu.net>
2026-03-03 00:44:53 +0000

    Skibidi

--- fractal/triangle.py
+++ fractal/triangle.py
@@ -2,6 +2,7 @@
 
 import turtle as trutle
 import time
+from helpers import e, s_triangle
 
 depth = int(input("choose depth 1-8: "))
 color = input("choose color: ")
@@ -14,41 +15,16 @@ length = 400
 
 
 #setup
-e = trutle.Turtle()
 e.width(1)
 e.speed(0)
 e.hideturtle()
 e.setpos(0,0)
 e.pencolor(color)
 screen.bgcolor("black")
-#infinite triangles that are half length of previous ones and half positions so it makes sierpinski triangle
-def triangle(length):
-    for i in range(3):
-        e.forward(length)
-        e.right(120) #turns 120 degrees to form 60 degree interior engle
-
-def s_triangle(length, length2):
-    if length2 == 0:
-        triangle(length)
-    else:
-        s_triangle(length/2, length2-1) 
-        e.forward(length/2) #move the psotition for new triangle
-        s_triangle(length/2, length2-1)
-        e.backward(length/2)
-        e.right(60)#top most riangle
-        e.forward(length/2)
-        e.left(60)
-        s_triangle(length/2, length2-1)
-        e.right(60)#resets
-        e.backward(length/2)
-        e.left(60)
-
 
 s_triangle(length, depth)
 
-
 screen.update()
-
 
 screen.exitonclick()
 #easy way toi exit
